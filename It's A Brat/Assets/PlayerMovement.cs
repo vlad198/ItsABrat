86a541053c875128582081db9ffc7993ca5f7ddf
Anechitei Diana <anechitei27@gmail.com>
2022-03-15 18:28:03 +0200

    add animation iddle and running

--- It's A Brat/Assets/PlayerMovement.cs
+++ It's A Brat/Assets/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,19 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(0,14,0);
+        }
+
+        if(dirX > 0f) // running to the right
+        {
+            anim.SetBool("running", true);
+        }
+        else if(dirX < 0f) // running to the left
+        {
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
