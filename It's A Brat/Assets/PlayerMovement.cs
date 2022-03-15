030b410ea8957eec15845dd4570ad2f0481a8589
unknown <mihaelachira8@gmail.com>
2022-03-15 22:25:41 +0200

    added collectibles items

--- It's A Brat/Assets/PlayerMovement.cs
+++ It's A Brat/Assets/PlayerMovement.cs
@@ -6,11 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableGround;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -20,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(0,14,0);
         }
@@ -37,5 +40,10 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("running", false);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
